772096bb5a0aa367069207ee560dfe5a1d9d7295
hwangseon-a <tjsdk2769@gmail.com>
2018-11-19 12:02:47 +0900

    9 답변쓰기 구현

---
+++ src/mvjsp/chap17/board/service/CannotReplyArticleException.java
@@ -0,0 +1,7 @@
+package mvjsp.chap17.board.service;
+
+public class CannotReplyArticleException extends Exception {
+	public CannotReplyArticleException(String message) {
+		super(message);
+	}
+}
---
+++ src/mvjsp/chap17/board/service/LastChildAlreadyExistException.java
@@ -0,0 +1,7 @@
+package mvjsp.chap17.board.service;
+
+public class LastChildAlreadyExistException extends Exception{
+	public LastChildAlreadyExistException(String message) {
+		super(message);
+	}
+}
